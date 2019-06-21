d4e41440ba969722f5fe1d51d33ecbe9e94e6632
erbcreedok <erbcreedok@gmail.com>
2019-06-21 22:49:05 +0600

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,261 @@
 <template>
     <div class="home">
-        <jumbotron/>
+        <section class="hp_hero">
+            <div class="wrap hp_hero_wrap">
+                <div class="hp_hero_content">
+                    <div class="hp_hero_title">ATAMEKEN</div>
+                    <div class="hp_hero_subtitle">
+                        <span class="txt-rotate" data-period="1000">{{txtAnimate}}</span>
+                        для начинающих и действующих <br>предпринимателей
+                    </div>
+                    <div class="hp_hero_tags">
+                        <a href="#" class="button button-small button-primary button-inverse">Старт бизнеса</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Операционка</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Персонал</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Финансы</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Маркетинг</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Продажи</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Управление рисками</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Право</a>
+                        <a href="#" class="button button-small button-primary button-inverse">Развитие бизнеса</a>
+                    </div>
+                </div>
+
+                <div class="hp_hero_illustration"></div>
+            </div>
+        </section>
+        <div class="wrap">
+            <section class="hp_section">
+                <div class="hp_title">Курсы и материалы</div>
+
+                <form class="hp_search textfield textfield-block">
+                    <input type="text" class="qa_search_field textfield_input" placeholder=" ">
+                    <label class="textfield_label">Ищите ответы на свои вопросы или интересующие материалы</label>
+                    <button type="submit" class="textfield_button textfield_button-search"></button>
+                </form>
+
+                <div class="hp_content blog_grid blog_grid-static">
+                    <article class="blogcard blogcard-wide blogcard-hl blogcard-inverse">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Онлайн-курс</div>
+                            <a href="#" class="blogcard_title">Подготовка к открытию своего дела</a>
+
+                            <ul class="blog_stats blogcard_stats">Бесплатный онлайн-курс из 12 занятий</ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Как не надо размещать рекламу у блогеров</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Бизнес-словарь. Доход, выручка, прибыль – простыми словами.</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-wide blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Первый шаг к тому, чтобы начать бизнес. Just do it!</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-wide blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Золотое правило рекламы – знай своего клиента в лицо или история Malboro</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="quizblock">
+                        <div class="quizblock_illustration">
+                            <img src="/assets/img/illustration/quiz.png" alt="">
+                        </div>
+
+                        <div class="quizblock_content">
+                            <div class="quizblock_title">Готовы ли вы начать собственный бизнес?</div>
+
+                            <div class="quizblock_description">Реализовать давнюю мечту, заниматься тем, что действительно нравится, зарабатывать хорошие деньги и приносить людям радость с помощью своих замечательных и невероятно полезных товаров и услуг… Звучит здорово! А думали ли вы всерьез о том, насколько ваша бизнес-идея жизнеспособна, и есть ли у вас достаточно знаний и упорства для ее реализации?</div>
+
+                            <div class="quizblock_controls">
+                                <a href="#" class="button button-primary button-outline button-icon button-icon-right button-icon-arrow-right">Пройти тест</a>
+                                <span>15 вопросов &nbsp; • &nbsp; 3 минуты</span>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a aliquid hic reprehenderit quos harum amet!</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a aliquid hic reprehenderit quos harum amet!</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a aliquid hic reprehenderit quos harum amet!</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+
+                    <article class="blogcard blogcard-inverse blogcard-stackedMobile">
+                        <div class="blogcard_cover">
+                            <img src="http://placehold.it/500x500" alt="">
+                        </div>
+
+                        <div class="blogcard_wrap">
+                            <div class="blogcard_type">Статья</div>
+                            <div class="blogcard_category">Личная эффективность</div>
+                            <a href="#" class="blogcard_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a aliquid hic reprehenderit quos harum amet!</a>
+
+                            <ul class="blog_stats blogcard_stats">
+                                <li class="blog_stat blog_stat-rating">13</li>
+                                <li class="blog_stat blog_stat-comments">1</li>
+                                <li class="blog_stat blog_stat-bookmarks">12</li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+
+                <a href="#" class="hp_section_button button button-primary button-outline button-icon button-icon-right button-icon-arrow-right">Все материалы</a>
+            </section>
+        </div>
     </div>
 </template>
 
 <script>
-    import Jumbotron from "../sections/Jumbotron";
-
     export default {
-      components: {Jumbotron}
+      data() {
+        return {
+          txtActive: 0,
+          txtLength: 0,
+          txtDirection: 1,
+          txtArray: [ "медиа", "сообщество", "образование" ],
+          txtDuration: 100,
+        }
+      },
+      methods: {
+        changeTxt() {
+          setTimeout(() => {
+            if (this.txtDirection > 0) {
+              this.txtLength += 1;
+            }
+            if (this.txtDirection < 0) {
+              this.txtLength -= 1;
+            }
+            if (this.txtLength === this.txtArray[this.txtActive].length + 1) {
+              this.txtDirection = -1;
+            }
+            if (this.txtLength === 0) {
+              this.txtDirection = 1;
+              this.txtActive += 1;
+              this.txtActive %= this.txtArray.length;
+            }
+            this.txtDuration = Math.random() * 300 + this.txtDirection * 100;
+            this.changeTxt();
+          }, this.txtDuration)
+        }
+      },
+      computed: {
+        txtAnimate() {
+          return this.txtArray[this.txtActive].substr(0, this.txtLength);
+        }
+      },
+      mounted() {
+        this.changeTxt();
+      },
     }
 </script>
